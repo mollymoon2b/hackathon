2d4613dedd2447c3b34777062eefb01b207a7bb9
Alice DE-BONNECHOSE <ade-bonn@e2r1p7.42.fr>
2015-02-22 10:27:17 +0100

    alice

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -1,10 +1,30 @@
-/**
+﻿/**
  * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	config.language = 'fr';
+	config.uiColor = '#FF9128';
+	// %REMOVE_START%
+	config.plugins =
+
+		'basicstyles,' +
+		'colorbutton,' +
+		'colordialog,' +
+		'contextmenu,' +
+		'dialogadvtab,' +
+		'elementspath,' +
+		'font,' +
+		'format,' +
+		'resize,' +
+		'showblocks,' +
+		'sourcearea,' +
+		'toolbar,' +
+		'image,' +
+		'wysiwygarea';
+	// %REMOVE_END%
 };
+
+// %LEAVE_UNMINIFIED% %REMOVE_LINE%
